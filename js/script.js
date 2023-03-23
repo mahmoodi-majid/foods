48103167402d2f6c9c5a385e987dc569fb61f8be
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2023-03-24 03:44:30 +0430

    add function add user to card

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ const AllProduct = [
     Price: 33,
     imgSrc: "img/ghormeSabzi.jpg",
     avout :
-      " This Iranian food is very delicious Local meat and vegetables areused in this dishPrice: 33 $",
+      "This Iranian food is very delicious Local meat and vegetables areused in this dishPrice: 33 $",
+      count: 1
   },
   {
     id: 2,
@@ -16,6 +17,7 @@ const AllProduct = [
     imgSrc: "img/kabab.jpg",
     about :
       "This dish is very delicious. In this dish, minced meat is skewered The price is$40",
+      count: 1
   },
   {
     id: 3,
@@ -24,6 +26,7 @@ const AllProduct = [
     imgSrc: "img/gheyme.jpg",
     about :
       "It is a very delicious dish that has meat, cobs, and fried Zemini apples The price is The price is$49",
+      count: 1
   },
   {
     id: 4,
@@ -32,6 +35,7 @@ const AllProduct = [
     imgSrc: "img/bamiye.jpg",
     about :
       "There is meat in this okra stew and Iranians love this dish very much The price is$47",
+      count: 1
   },
   {
     id: 5,
@@ -40,6 +44,7 @@ const AllProduct = [
     imgSrc: "img/mahi.jpg",
     about :
       "Fish is very popular in Iran and most Iranians include it in their diet The price is$60",
+      count: 1
   },
   {
     id: 6,
@@ -48,6 +53,7 @@ const AllProduct = [
     imgSrc: "img/morgh.jpg",
     about :
       "In Iran, peoples consumption of chicken is very high and they like it very much and cook it in their parties The price is$70 ",
+      count: 1
   },
   {
     id: 7,
@@ -56,6 +62,7 @@ const AllProduct = [
     imgSrc: "img/sabzococo.jpg",
     about :
       "This food is very useful for health and is very popular in Iran The price is $30",
+      count: 1
   },
   {
     id: 8,
@@ -64,11 +71,13 @@ const AllProduct = [
     imgSrc: "img/ashReshte.jpg",
     about :
       "This dish is very popular in Iran and is eaten in cold weather The price is $40",
+      count: 1
   },
 ];
 
 let userBasket = []
 const mineContent = $.querySelector(".mine-content");
+const cartItems = $.querySelector(".cart-items")
 
 AllProduct.forEach((Product) => {
   let contentBoxFoods = $.createElement("div");
@@ -108,7 +117,7 @@ function findProduct (ProductId) {
   let SercheProduct =  AllProduct.find((Product)=> {
     return ProductId === Product.id
   })
-  console.log(SercheProduct);
+  
   userBasket.push(SercheProduct)
   addUserBasketToCard(userBasket)
 
@@ -116,4 +125,49 @@ function findProduct (ProductId) {
 
 function addUserBasketToCard (userBasketPrduct) {
   console.log(userBasketPrduct);
+  cartItems.innerHTML = ""
+  userBasketPrduct.forEach(function(Product){
+    console.log(Product);
+    let cartRow = $.createElement("div")
+  cartRow.classList = "cart-row"
+
+  let cartItem = $.createElement("div")
+  cartItem.classList = 'cart-item cart-column'
+
+  let cartItemImage = $.createElement("img")
+  cartItemImage.setAttribute('src', Product.imgSrc)
+  cartItemImage.classList = 'cart-item-image'
+  cartItemImage.setAttribute("height", "100");
+  cartItemImage.setAttribute("width", "100");
+  
+
+  let cartItemTitle = $.createElement("span")
+  cartItemTitle.classList = 'cart-item-title'
+  cartItemTitle.innerHTML = Product.ProductName
+  
+  let cartPrice = $.createElement("span")
+  cartPrice.classList = 'cart-price cart-column'
+  cartPrice.innerHTML = Product.Price
+
+  let cartQuantity = $.createElement("div")
+  cartQuantity.classList = 'cart-quantity cart-column'
+
+  let cartQuantityInput = $.createElement("input")
+  cartQuantityInput.classList = 'cart-quantity-input'
+  cartQuantityInput.setAttribute("type" , 'number')
+    cartQuantityInput.value = Product.count
+
+
+  let btnDanger = $.createElement("button")
+  btnDanger.classList = 'btn btn-danger' 
+  btnDanger.innerHTML = 'Remove'
+
+
+  cartItem.append(cartItemImage,cartItemTitle)
+  cartQuantity.append(cartQuantityInput,btnDanger)
+  cartRow.append(cartItem,cartPrice,cartQuantity)
+  cartItems.append(cartRow)
+  
+  })
+  
 }
